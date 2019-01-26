2860018804cc78475bcbf9d954ed3b6038c2f3b2
alexanderqchen <aqchen@ucla.edu>
2019-01-26 15:22:44 -0800

    Updates

---
+++ precache-manifest.01acae36420bf9976a1382079186f620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa16f34d40048460630",
+    "url": "/ctf-website-2019/static/css/main.781203b5.chunk.css"
+  },
+  {
+    "revision": "2aa16f34d40048460630",
+    "url": "/ctf-website-2019/static/js/main.2aa16f34.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/ctf-website-2019/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "36ddeb2804b016b51110",
+    "url": "/ctf-website-2019/static/js/runtime~main.36ddeb28.js"
+  },
+  {
+    "revision": "f809e0003311a99f5c4226f2c2b060dc",
+    "url": "/ctf-website-2019/index.html"
+  }
+];
